d1c25193a21016b0c3d9c7568afcce2e0f65bada
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-01 09:23:23 +0700

    1.0.0

---
+++ FadeScaleTransition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityExtensions;
+using Widgets;
+
+namespace WidgetTransitions
+{
+    public class FadeScaleTransition : MonoBehaviour
+    {
+        [SerializeField] private Image background;
+        [SerializeField] private Transform layout;
+        [SerializeField] private ScriptableObjectGraphicRaycaster raycaster;
+        [SerializeField] private UnityWidget widget;
+        [SerializeField] private ScriptableObjectWidgetFactory widgetFactory;
+        
+        public void Hide()
+        {
+            StartCoroutine(HideAsync());
+        }
+
+        private IEnumerator HideAsync()
+        {
+            raycaster.Enabled = false;
+            yield return FadeScaleUtils.HideAsync(background.SetAlpha, layout.SetLocalScale);
+            widgetFactory.DestroyWidget(widget);
+            raycaster.Enabled = true;
+        }
+    }
+}
